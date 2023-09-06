e77ba4cc8ac15811fa71f475d9718ddd249f8303
MortuusBlasphemer <wagutierrez0707@gmail.com>
2023-09-06 15:55:02 -0400

    ReVamp and port

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Phone Store</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<?php require_once 'layout/navbar.php'; ?>
+<body>
+
+
+<br><br>
+<h1 class="center">Productos principales</h1>
+<br><br>
+
+
+</body>
+</html>
